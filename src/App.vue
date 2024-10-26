1d23917ef8a9b0cb535801a698a16261b9976178
Jackie Yang <yang192827@163.com>
2024-10-26 21:47:42 +0800

    init tailwind css

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <h1 class="bg-amber-50 underline">
+    This is the app view
+  </h1>
   <RouterView />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import './assets/base.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
